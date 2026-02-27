72e9cbf2bb79cfef8270d4651f80197548130d6b
Fabio Prizio <fabio.prizio@prima.it>
2026-02-27 12:11:59 +0100

    feat: add Task data class with constructor and a complete function

--- src/main/kotlin/com/tasktracker/domain/Task.kt
+++ src/main/kotlin/com/tasktracker/domain/Task.kt
@@ -1,3 +1,35 @@
 package com.tasktracker.domain
 
-class Task {}
+import java.util.UUID
+import kotlin.time.Clock
+import kotlin.time.Instant
+
+@ConsistentCopyVisibility
+data class Task private constructor(
+    val id: UUID,
+    val user: String,
+    val title: String,
+    val description: String,
+    val isCompleted: Boolean,
+    val creationDate: Instant,
+    val completionDate: Instant?,
+) {
+    companion object {
+        fun create(
+            id: UUID = UUID.randomUUID(),
+            user: String,
+            title: String,
+            description: String = ""
+        ) = Task(
+            id = id,
+            user = user,
+            title = title,
+            description = description,
+            creationDate = Clock.System.now(),
+            isCompleted = false,
+            completionDate = null
+        )
+
+    }
+    fun complete() = copy(isCompleted = true, completionDate = Clock.System.now())
+}
